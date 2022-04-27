aa9ad2f99e44a661bccc099dc88490742291ac82
Anoskh <kimhab.zeecode@gmail.com>
2022-04-27 23:13:08 +0700

    03. C Syntax, Output (Newline and More) Khmer - HKimhab

--- CCrashCourse/main.c
+++ CCrashCourse/main.c
@@ -4,5 +4,8 @@
 int main()
 {
     printf("Hello Universe from HKimhab!\n");
+    printf("\tThis C crash course by \"HKimhab\"");
+    printf("\nI'm HKimhab and Recording C Crash Course\n");
+    printf("Hello Crush nice to know you \"DSreyKhuoch\"");
     return 0;
 }
