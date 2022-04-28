a1485d822683812dd4ce05909ccf9ca0b30311a0
Anoskh <kimhab.zeecode@gmail.com>
2022-04-28 22:24:58 +0700

    04. C Comments and C Variable Khmer HKimhab

---
+++ main2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Learning comment in c now
+    /*
+        printf("Hello Universe from HKimhab!\n");
+        return 0;
+    */
+    int age = 10;
+    char myname = 'H';
+    int _age = 22;
+    int _1age = 22;
+
+    // printf("%i", variable);
+    printf("%i\n", _1age);
+    printf("%c\n", myname);
+
+    // Declare variable of string
+    char fN[] = "HKimhab";
+    printf("%s\n", fN);
+
+    char greetings[] = "Hello Universe!";
+    printf("%s\n", greetings);
+
+    printf("Hello comment from your Crush.");
+    return 0;
+}
